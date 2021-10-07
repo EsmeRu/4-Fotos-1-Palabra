a694b3070f8bfe6355de9f851388a391a94bc09b
ChristianSalasInzunza <chsa_18@alu.uabcs.mx>
2021-10-07 17:03:16 -0600

    segunda activity

---
+++ app/src/main/java/com/movil/application/PantallaInicio.kt
@@ -0,0 +1,11 @@
+package com.movil.application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PantallaInicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_inicio)
+    }
+}
